082fdfb33c40fa48a2ab89263e680cae6e52d460
rawarne2 <rashaunwarner@yahoo.com>
2018-07-17 12:27:49 -0500

    functioning wheel and food entry

--- components/Wheel.js
+++ components/Wheel.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import { StyleSheet, View, Animated, Easing, Image, TouchableHighlight, Text as RNText} from 'react-native';
+import { StyleSheet, View, Animated, Easing, Image, 
+        TouchableHighlight, Text as RNText, Platform, 
+        TextInput, KeyboardAvoidingView } from 'react-native';
 import { PieChart } from 'react-native-svg-charts';
 import { Text } from 'react-native-svg'
 
 const timing = 1000
 let position
+let key = 0
 
 export default class Wheel extends React.Component {
     constructor (props) {
         super(props)
         this.state = {
-            isSpinning: false
+            isSpinning: false,
+            text: '',
+            data: [{
+                key: 0,
+                amount: 50,
+                svg: { fill: '#9900cc' }, 
+                name: 'Enter Food'
+            }]
         }
         this.spinValue = new Animated.Value(0)
         this.spin = this.spin.bind(this)
         this.toggleSpin = this.toggleSpin.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.clear = this.clear.bind(this)
       }
     
       spin () {
-        console.log('IT SHOULD BE SPINNING!!!')
         this.spinValue.setValue(0)
         Animated.timing(
           this.spinValue,
@@ -33,11 +44,10 @@ export default class Wheel extends React.Component {
                 // this.spinValue.addListener((val)=> {console.log(val)})
             }
             else {
-              this.spinValue.setValue(position)   //add decay? to make to slow down smoothly
+              this.spinValue.setValue(position)   //add decay? to make to slow down smoothly. make it obvious that it is slowing down
             }
         })
       }
-
 
         toggleSpin () {
             position = this.spinValue._value
@@ -46,43 +56,69 @@ export default class Wheel extends React.Component {
                 isSpinning: !prevState.isSpinning
             }))
         }
-      
-      render() {
-         
-        const data = [
-            {
-                key: 1,
-                amount: 50,
-                svg: { fill: '#600080' },
-                name: 'McDonalds',
-                
-            },
-            {
-                key: 2,
-                amount: 50,
-                svg: { fill: '#9900cc' }, 
-                name: 'Make Food'
-            },
-            {
-                key: 3,
-                amount: 50,
-                svg: { fill: '#c61aff' }, 
-                name: 'Chipotle'
-            },
-            {
-                key: 4,
-                amount: 50,
-                svg: { fill: '#d966ff' },
-                name: 'Hibachi'
-            },
-            {
-                key: 5,
-                amount: 50,
-                svg: { fill: '#ecb3ff' },
-                name: 'Chilis'
-            }
-        ]
 
+        handleSubmit() {
+            if (this.state.data[0].key === 0) {
+                this.state.data.pop()
+            }
+            this.setState({ 
+                data: this.state.data.concat(                {
+                    key: ++key,
+                    amount: 50,
+                    svg: { fill: '#600000' },
+                    name: this.state.text
+                })
+            })
+            this.setState({ text: '' })
+        } 
+      
+        clear() {
+            this.setState({
+                data: [{
+                    key: 0,
+                    amount: 50,
+                    svg: { fill: '#9900cc' }, 
+                    name: 'Enter Food'
+                }]
+            })
+        }
+
+      render() {
+         console.log(this.state.text)
+        // const data = [
+        //     {
+        //         key: 1,
+        //         amount: 50,
+        //         svg: { fill: '#600080' },
+        //         name: 'McDonalds',
+                
+        //     },
+            // {
+            //     key: 2,
+            //     amount: 50,
+            //     svg: { fill: '#9900cc' }, 
+            //     name: 'Make Food'
+            // },
+        //     {
+        //         key: 3,
+        //         amount: 50,
+        //         svg: { fill: '#c61aff' }, 
+        //         name: 'Chipotle'
+        //     },
+        //     {
+        //         key: 4,
+        //         amount: 50,
+        //         svg: { fill: '#d966ff' },
+        //         name: 'Hibachi'
+        //     },
+        //     {
+        //         key: 5,
+        //         amount: 50,
+        //         svg: { fill: '#ecb3ff' },
+        //         name: 'Chilis'
+        //     }
+        // ]
+        let data = this.state.data
         const Labels = ({ slices, height, width }) => {
             return slices.map((slice, index) => {
                 const { labelCentroid, pieCentroid, data } = slice;
@@ -111,12 +147,14 @@ export default class Wheel extends React.Component {
             outputRange: ['0deg', '360deg']
           })
         return (
-            <View>
+            <KeyboardAvoidingView behavior="position" enabled>
             <Image source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Red_Arrow_Down.svg/600px-Red_Arrow_Down.svg.png'}}
                    style={{width: 40, height: 50, alignSelf: 'center'}}
                    />
             <Animated.View className="spinner" style={{  transform: [{'rotate': spin}],
-            // perspective: 1000     // without this line this Animation will not render on Android while working fine on iOS. Make ternary for if its an Android
+            ...Platform.select({ 
+                android: { perspective: 1000 }  // without this line this Animation will not render on Android while working fine on iOS. Make ternary for if its an Android
+            })     
             }}>
             <PieChart
                 style={{ height: 350, width:350}}
@@ -128,11 +166,19 @@ export default class Wheel extends React.Component {
                 <Labels/>
             </PieChart>
             </Animated.View>
-            <TouchableHighlight style={{backgroundColor: 'gray', height: 75, width: 250}} 
+            <TouchableHighlight style={{backgroundColor: 'gray', height: 50, width: 150}} 
                 onPress={this.toggleSpin }>
                 <RNText>{this.state.isSpinning ? 'Stop' : 'Start'}</RNText>
             </TouchableHighlight>
-            </View>
+            <TouchableHighlight style={{backgroundColor: 'yellow', height:50, width:150}}
+                onPress={this.clear}>
+                <RNText>Clear</RNText>
+            </TouchableHighlight>
+            <TextInput style={{height: 40}} placeholder="ENTER RESTAURANT HERE"  
+                       onChangeText={(text) => this.setState({text})}
+                       onSubmitEditing={this.handleSubmit}
+                       value={this.state.text}/>
+            </KeyboardAvoidingView>
         )
     }
 }
@@ -140,3 +186,7 @@ export default class Wheel extends React.Component {
 //generate a random color for pie chart
 //map through a list
 //make it start and stop on the touchablehighlight
+// add a "remove" dropdown list of what has been enterd
+//store the data on whatever is equivalient to local storage for react native
+//styleing
+//
